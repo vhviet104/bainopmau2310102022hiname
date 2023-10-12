3639810d36fd0a5ca41c3e5a2712533a97ba9c2b
vhviet104 <147029729+vhviet104@users.noreply.github.com>
2023-10-12 06:44:15 +0000

    nopbaitap

--- s00_bailam.py
+++ s00_bailam.py
@@ -33,6 +33,12 @@ hi('1', '22', '333', '4444') | Hi 1, 22, 333 and 4444!
 #endregion debai
 
 #region bailam
-def hi(name):
-  return 'todo'
+def hi(name = ''):
+  if name == 'Mom':
+    return f'Hi {name}!'
+  else: 
+    return 'Hi!'
+
+    
+
 #endregion bailam
